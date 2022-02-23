988c1f66bdaa994f3d1c41a215b07f08d023a440
Jesus Martinez Caudillo <martinez@Jesuss-Mac-mini.local>
2022-02-23 07:18:32 -0500

    add frequency.py from Andy example

---
+++ frequency.py
@@ -0,0 +1,10 @@
+def getfreq(loweronly):
+    frequency = {}
+    for ascii in range(ord('a'), ord('a') + 26):
+        frequency[chr(ascii)] = float(loweronly.count(chr(ascii))) / len(loweronly)
+
+    sum_freq_squared = 0.0
+    for ltr in frequency:
+        sum_freq_squared += frequency[ltr] * frequency[ltr]
+
+    return sum_freq_squared
